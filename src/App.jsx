3ef8d63f1e54d9113bab579ddd70418c28515058
fayazap <fayazap2@gmail.com>
2024-06-22 13:09:20 +0530

    navbar and hero

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+
+
+const App = () => {
+
+  return (
+    <main>
+      <Navbar />
+      <Hero />
+      <Highlights />
+    </main>
+  )
+}
+
+export default App
